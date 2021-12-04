ce562d10b70d1586bf5bf8ca79fb2580acd2809a
nefri <nefri@yandex-team.ru>
2021-12-04 18:19:48 +0300

    добавляет отрисовку первого компонента ня сервере

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client')
+    })
+  ],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
